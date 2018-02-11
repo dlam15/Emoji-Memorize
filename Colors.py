0926e45266f6519d4790708b5c3b5c205adbdbee
dlam15 <31551025+dlam15@users.noreply.github.com>
2018-02-11 05:19:43 -0500

    emoji changer

---
+++ Colors.py
@@ -0,0 +1,36 @@
+import pygame
+import random
+
+
+class Colors(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.x = x
+        self.y = y
+        #self.name = name
+        #self.image = pygame.Surface((100,100))
+        self.emoji = ['trollemoji.png','happyemoji.jpg','sademoji.png', \
+                      'cryingemoji.png','laughingemoji.png','smirkemoji.png', \
+                      'kissemoji.png', 'scaredemoji.png','loveemoji.png']
+
+        self.image = pygame.image.load('neutralemoji.png')
+        self.image = pygame.transform.scale(self.image, (100,100))
+        self.rect = self.image.get_rect(topleft=(self.x,self.y))
+
+    def flash(self):
+        self.image = pygame.image.load(random.choice(self.emoji))
+        self.image = pygame.transform.scale(self.image, (100,100))
+
+    def revert(self):
+        self.image = pygame.image.load('neutralemoji.png')
+        self.image = pygame.transform.scale(self.image, (100,100))
+
+    def wrong(self):
+        self.image = pygame.image.load('devilemoji.png')
+        self.image = pygame.transform.scale(self.image, (100,100))
+        
+    def right(self):
+        self.image = pygame.image.load('angelemoji.png')
+        self.image = pygame.transform.scale(self.image, (100,100))
+        
